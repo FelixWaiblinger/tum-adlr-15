68427f9b94a2344870cf37f1985502023f2943a9
Niklas Schroeder <niklasusa20@gmail.com>
2024-07-11 21:18:17 +0200

    bps to img prediction

--- bps_to_image_prediction/train_bps_to_image_nn.py
+++ bps_to_image_prediction/train_bps_to_image_nn.py
@@ -12,12 +12,12 @@ import queue
 import torch
 
 BATCH_SIZE = 128
-DEVICE = "cuda"
-EPOCHS = 20
-N_SAMPLES = 10000
+DEVICE = "gpu" if torch.cuda.is_available() else "cpu"
+EPOCHS = 200
+N_SAMPLES = 50000
 IMG_DATA_LOCATION = "./bps_img_img.pt"
 BPS_DATA_LOCATION = "./bps_img_bps.pt"
-
+CHECKPOINT_LOCATION = r"D:\uni\adl4r\code\tum-adlr-15\bps_to_image_prediction\lightning_logs\version_18\checkpoints\epoch=199-step=70400.ckpt"
 
 def training_lightning():
     """Trains a nn that maps from bps to images"""
@@ -38,13 +38,14 @@ def training_lightning():
         shuffle=True
     )
     model = LitBpsToImgNetwork()
-    trainer = L.Trainer()
+    trainer = L.Trainer(accelerator=DEVICE, max_epochs=EPOCHS)
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
 
 def eval_ligthning():
     # load dataset
     dataset = BpsToImgDataset(img_data=IMG_DATA_LOCATION, bps_data=BPS_DATA_LOCATION, img_transform=BinarizeTransform())
+
 
     # split up into training and test dataset
     train_idx, val_idx = train_test_split(range(N_SAMPLES), test_size=0.1)
@@ -59,15 +60,17 @@ def eval_ligthning():
         shuffle=True
     )
 
-    model = LitBpsToImgNetwork.load_from_checkpoint(
-        r"F:\Uni\master\Semester 2\ADL4R\project\codebase\tum-adlr-15\bps_to_image_prediction\lightning_logs\version_6\checkpoints\epoch=216-step=15407.ckpt")
+
+    model = LitBpsToImgNetwork.load_from_checkpoint(CHECKPOINT_LOCATION).cpu()
     model.eval()
     data_iter = iter(val_loader)
     for l in data_iter:
-        features, labels = l
+
+        features, labels, original_labels = l
+
         prediction = model(features)
         num_comparisons = 3
-        fig, ax = plt.subplots(num_comparisons, 2)
+        fig, ax = plt.subplots(num_comparisons, 3)
         for i in range(num_comparisons):
             # plot both images
             img = labels[i]
@@ -89,12 +92,17 @@ def eval_ligthning():
             ax[i, 1].set_title(f'Label: Reconstruction')
             ax[i, 1].axis('off')  # Turn off axis
 
+            img3 = original_labels[i]
+            img3 = img3.detach().numpy().transpose(1, 2, 0)
+            ax[i, 2].imshow(img3)  # Use squeeze to remove channel dimension
+            ax[i, 2].set_title(f'Label: RGB_Image')
+            ax[i, 2].axis('off')  # Turn off axis
+
         plt.show()
 
 
 def ingame_evaluation():
-    model = LitBpsToImgNetwork.load_from_checkpoint(
-        r"F:\Uni\master\Semester 2\ADL4R\project\codebase\tum-adlr-15\bps_to_image_prediction\lightning_logs\version_6\checkpoints\epoch=216-step=15407.ckpt")
+    model = LitBpsToImgNetwork.load_from_checkpoint(CHECKPOINT_LOCATION).cpu()
     model.eval()
     options = OPTIONS
     options.update({"render": "rgb_array"})
@@ -118,9 +126,10 @@ def ingame_evaluation():
             fig, ax = plt.subplots(1, 2)
             ax[0].imshow(prediction, cmap='gray')
             img = env.render()
-            img = img[2::8, 2::8, :].transpose([1, 0, 2])
+            img = img[2::8, 2::8, :]
             ax[1].imshow(img)
             plt.show()
+            _ = env.reset()
             continue
 
         if not terminated:
@@ -131,7 +140,27 @@ def ingame_evaluation():
                 lq.get()
 
 
+def check_dataset():
+    img_data_location = "./bps_img_img.pt"
+    bps_data_location = "./bps_img_bps.pt"
+    dataset = BpsToImgDataset(img_data=img_data_location, bps_data=bps_data_location, img_transform=BinarizeTransform())
+
+    train_dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    train_features, train_labels = next(iter(train_dataloader))
+    for i in range(64):
+
+        print(f"Feature batch shape: {train_features.size()}")
+        print(f"Labels batch shape: {train_labels.size()}")
+        img = train_labels[i].numpy()
+        plt.imshow(img, cmap="gray")
+        plt.show()
+
+
+
+
+
 if __name__ == "__main__":
-    training_lightning()
-    # eval_ligthning()
-    # ingame_evaluation()
+    #training_lightning()
+    #eval_ligthning()
+    ingame_evaluation()
+    #check_dataset()
